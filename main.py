115749a73f339d2428f341b9c1d74750c6d773fb
saranya <saranyab@bay-infotech.com>
2020-10-27 12:43:59 -0700

    Initial Code - Login Page
    Initial Code - Login Page

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,request
+import mysql.connector
+
+app=Flask(__name__)
+
+#conn=mysql.connector.connect(host=)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def about():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    return "The email is {} and the password is {}".format(email,password)
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
+   
+
+   # flask with ML
+   #https://www.youtube.com/watch?v=UbCWoMf80PY&feature=emb_logo
